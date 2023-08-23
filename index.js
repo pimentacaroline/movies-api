d6b90222977fa7a8ecb4dd55315a2f507dc19030
pimentacaroline <pimenta.caroline@gmail.com>
2023-08-23 12:34:02 +0200

    Connected Database to API

--- index.js
+++ index.js
@@ -18,7 +18,11 @@ const accessLogStream = fs.createWriteStream(
 	{flags: 'a'}
 );
 
-mongoose.connect('mongodb://localhost:27017/cpdb', {useNewUrlParser:true, useUnifiedTopology:true});
+// mongoose.connect('mongodb://localhost:27017/cpdb', {useNewUrlParser:true, useUnifiedTopology:true});
+
+mongoose.connect('process.env.CONNECTION_URI', {useNewUrlParser:true, useUnifiedTopology:true});
+
+
 
 app.use(bodyParser.json());
 app.use(express.json());
