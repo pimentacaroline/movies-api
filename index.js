d93bb00581bb3537a68d5df8e4b0a0c7520a978d
pimentacaroline <pimenta.caroline@gmail.com>
2023-08-30 17:29:20 +0200

    Revert "test"
    This reverts commit dfa4318fb9fc8b15cf6f6c50fa3f2dd331b5f611.

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const passport = require('passport');
 require('./passport');
 
 app.get('/', (req, res) => {
-  res.send('Testing my welcome page');
+  res.send('Welcome to my movie app.');
 });
 
 // #1 Return a list of ALL movies
